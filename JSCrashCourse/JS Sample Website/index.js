750b2821feb419898b02c487e4e7f3353017fd4b
Ryan Norin <nironr6@gmail.com>
2022-05-17 13:54:01 +1000

    New Form 0.1

---
+++ JSCrashCourse/JS Sample Website/index.js
@@ -0,0 +1,14 @@
+/* document.getElementById( "count-el") .innerText = 5 */
+
+/* function increment () {
+    console.log ("The button was clicked!") */
+
+function countdown() {
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+}
+
+countdown()
