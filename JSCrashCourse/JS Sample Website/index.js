aed8cdd33b8e50ee093195cedf5d0b840c6a8d72
Ryan Norin <nironr6@gmail.com>
2022-05-19 13:24:47 +1000

    New changes 19/05/22

--- JSCrashCourse/JS Sample Website/index.js
+++ JSCrashCourse/JS Sample Website/index.js
@@ -1,16 +1,20 @@
-let num1 = 9
-let num2 = 10
+let firstCard = 11
+let secondCard = 10
+let sum = firstCard + secondCard
 
 
 document.getElementById("num1-el").textContent = firstCard
 document.getElementById("num2-el").textContent = secondCard
+document.getElementById("sum-el").textContent = sum
 
-let sumEl = document.getElementById("sum-el")
-
-
-function hit() {
-let sumEl.textContent = firstCard + secondCard
+if (sum < 21) {
+    console.log ("Would you like to draw a new card?")
+} else if (sum === 21) {
+    console.log ("Woohoo! You've got Blackjack!")
+} else if (sum > 21) {
+    console.log ("You're out of the game! Tough luck!")
 }
+
 
 
 
