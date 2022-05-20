ef31a16b10045a81a9dc8d18b9a9fc7fdcec62d7
Ryan Norin <nironr6@gmail.com>
2022-05-20 14:46:55 +1000

    new changes 2.0

--- JSCrashCourse/JS Sample Website/index.js
+++ JSCrashCourse/JS Sample Website/index.js
@@ -1,27 +1,32 @@
-let firstCard = 11
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
+let firstCard = 1
 let secondCard = 10
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
-
+let sumEl = document.getElementById ("sum-el")
 let messageEl = document.getElementById( "message-el")
+console.log (sumEl)
 
 function startGame() {
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Woohoo! You've got Blackjack!"
-    hasBlackjack = true
-} else { 
-    message = "Busted! You're out of the game!"
-    isAlive = false
-}
-}
-
-messageEl.textContent = message
-
-
-/*document.getElementById("num1-el").textContent = firstCard
-document.getElementById("num2-el").textContent = secondCard
-document.getElementById("sum-el").textContent = sum */
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+    } else { 
+        message = "Busted! You're out of the game!"
+        isAlive = false
+    }
+    }
+    
+    sumEl.textContent = "Sum: 14"
+    
+    
+    /*document.getElementById("num1-el").textContent = firstCard
+    document.getElementById("num2-el").textContent = secondCard
+    document.getElementById("sum-el").textContent = sum */
