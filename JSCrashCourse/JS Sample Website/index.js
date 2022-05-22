d27e516c4a9f20fe29ef5e0bee982a3716af29d9
Ryan Norin <nironr6@gmail.com>
2022-05-22 17:13:13 +1000

    New changes 30.

--- JSCrashCourse/JS Sample Website/index.js
+++ JSCrashCourse/JS Sample Website/index.js
@@ -1,32 +1,77 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-
-let firstCard = 1
-let secondCard = 10
-let sum = firstCard + secondCard
-let hasBlackjack = false
-let isAlive = true
-let message = ""
-let sumEl = document.getElementById ("sum-el")
-let messageEl = document.getElementById( "message-el")
-console.log (sumEl)
+/*
+firstCard = 10
+secondCard = 4
+cards = [firstCard, secondCard]
+sum = firstCard + secondCard
+hasBlackjack = false
+isAlive = true
+message = ""
+sumEl = document.getElementById ("sum-el")
+messageEl = document.getElementById( "message-el")
+sumEl = document.getElementById ( "sum-el")
+cardsEl = document.getElementById ("cards-el")
 
 function startGame() {
+    renderGame ()
+}
+
+function renderGame(){
+    cardsEl.textcontent = "Cards: "
+    for (let i= 0; i < cards.length; i++) {
+    }
+    
+    sumEl.textcontent= "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "You've got Blackjack!"
+        message = "You've got BlackJack!"
         hasBlackjack = true
-    } else { 
-        message = "Busted! You're out of the game!"
+    } else {
+        message = "You're out of the game!"
         isAlive = false
     }
+    messageEl.textcontent = message
+    
+} 
+
+function newCard () {
+    let card = 6
+    sum += card
+    cards.push (card)
+    console.log (cards)
+    renderGame ()
+}
+    
+*/
+
+player1Time = 102
+player2Time = 107
+
+/* function getFastestRaceTime() {
+    if (player1Time < player2Time) {
+        return player1Time
+    } else if (player2Time < player1Time) {
+        return player2Time
+    } else {
+        return player1Time
     }
+}
+
+fastestRace = getFastestRaceTime()
+
+console.log(getFastestRaceTime())
+
+*/
+
+function getTotalRaceTime() {
+    return player1Time + player2Time
+}
+
+totalTime = getTotalRaceTime ()
+
+console.log(totalTime)
+
+
     
-    sumEl.textContent = "Sum: 14"
     
     
-    /*document.getElementById("num1-el").textContent = firstCard
-    document.getElementById("num2-el").textContent = secondCard
-    document.getElementById("sum-el").textContent = sum */
