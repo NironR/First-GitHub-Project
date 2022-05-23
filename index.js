57efe0d65e31509a6ac1072b243b0a77263dda94
Ryan Norin <nironr6@gmail.com>
2022-05-23 12:32:53 +1000

    cahnge

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 let hasSolvedChallenge = false
 let hasHintsleft = false
 
-if (hasSolvedChallenge === false && hasHintsleft === false) {
+if (hasSolvedChallenge === false || hasHintsleft === false) {
     showSolution()
 }
 
 function showSolution () {
     console.log ("Showing the solution...")
+}
+
+
+let likesDocumentaries = true
+let likesStartups = true 
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie()
+}
+
+function recommendMovie(){
+    console.log ( "Hey! Check out this new film we think you will like!")
 }
