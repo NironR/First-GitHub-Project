f0e7fffc007d50e64961d63aab62e76ff227d1f8
Ryan Norin <nironr6@gmail.com>
2022-05-23 18:38:18 +1000

    Update 1.1

--- JSCrashCourse/JS Sample Website/Chrome Extension/index.js
+++ JSCrashCourse/JS Sample Website/Chrome Extension/index.js
@@ -1,5 +1,16 @@
+const inputEl = document.getElementById ( "input-el")
+const box = document.getElementById ("btn-main")
+const ulEl = document.getElementById ( "ul-el")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
+box.addEventListener ("click", function(){ 
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
 
-function saveLead(){
-    console.log ("button has been clicked!")
+let listItems = ""
+for (let i= 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + " " + "</li>"
 }
+
+ulEl.innerHTML = listItems
