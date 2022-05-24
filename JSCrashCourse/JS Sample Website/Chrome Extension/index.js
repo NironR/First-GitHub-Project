ad467b840e7492870d014b61db45d7b304ee2438
Ryan Norin <nironr6@gmail.com>
2022-05-24 17:44:36 +1000

    new update 1.3

--- JSCrashCourse/JS Sample Website/Chrome Extension/index.js
+++ JSCrashCourse/JS Sample Website/Chrome Extension/index.js
@@ -1,20 +1,27 @@
-const inputEl = document.getElementById ( "input-el")
-const box = document.getElementById ("btn-main")
-const ulEl = document.getElementById ( "ul-el")
+const inputEl = document.getElementById("input-el")
+const box = document.getElementById("btn-main")
+const ulEl = document.getElementById("ul-el")
 let myLeads = []
 
-box.addEventListener ("click", function(){ 
+box.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     renderLeads()
 })
 
 
-function renderLeads(){
+function renderLeads() {
     let listItems = ""
-    for (let i= 0; i < myLeads.length; i++) {
-        listItems += "<li> <a href= " + myLeads() + "' target= _blank> " + myLeads[i] + " " + "</a> </li>" 
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a href='${myLeads[i]}' target='_blank'> 
+                ${myLeads[i]}
+            </a>
+        </li>`
     }
 
     ulEl.innerHTML = listItems
 }
+
+
