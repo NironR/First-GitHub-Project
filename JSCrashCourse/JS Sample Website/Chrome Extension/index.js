1fdf89ba4b51b7acf60a13452319300281b626db
Ryan Norin <nironr6@gmail.com>
2022-05-24 17:09:08 +1000

    new update 1.2.1

--- JSCrashCourse/JS Sample Website/Chrome Extension/index.js
+++ JSCrashCourse/JS Sample Website/Chrome Extension/index.js
@@ -13,7 +13,7 @@ box.addEventListener ("click", function(){
 function renderLeads(){
     let listItems = ""
     for (let i= 0; i < myLeads.length; i++) {
-        listItems += "<li> <a href= '#' target= _blank> " + myLeads[i] + " " + "</a> </li>" 
+        listItems += "<li> <a href= " + myLeads() + "' target= _blank> " + myLeads[i] + " " + "</a> </li>" 
     }
 
     ulEl.innerHTML = listItems
