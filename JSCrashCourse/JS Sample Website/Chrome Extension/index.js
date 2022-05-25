1f3c76c6b0102a0990a48b63002c57b69fa310d3
Ryan Norin <nironr6@gmail.com>
2022-05-25 13:30:17 +1000

    update 1.5

--- JSCrashCourse/JS Sample Website/Chrome Extension/index.js
+++ JSCrashCourse/JS Sample Website/Chrome Extension/index.js
@@ -31,9 +31,11 @@ const tabs = [
 ]
 
 tabBtn.addEventListener("click", function () {
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
 
