fe66980d094ac823b0b1bdac9274a08303a6aaf1
Ryan Norin <nironr6@gmail.com>
2022-05-26 18:52:18 +1000

    new release 3.0

--- JSCrashCourse/JS Sample Website/JS Sample/app.js
+++ JSCrashCourse/JS Sample Website/JS Sample/app.js
@@ -1,12 +1,5 @@
-function getMax(seconds) {
-    let max = seconds[0];
-   for (i = 0; i < seconds.length; i++) {
-       if (seconds[i] > max) {
-           max = seconds[i]
-       }
-
-   }
-   return max
+function returnString(str) {
+    return str.split('').reverse('').join('')
 }
 
-console.log (getMax([-100, -200, -300])); 
+console.log(returnString('abc'))
