ce318296cd439c3a59bb4dd2d827f4f342b50371
Ryan Norin <nironr6@gmail.com>
2022-05-27 18:15:23 +1000

    new

--- JSCrashCourse/JS Sample Website/JS Sample/app.js
+++ JSCrashCourse/JS Sample Website/JS Sample/app.js
@@ -1,11 +1,5 @@
 function showRating(rating){
-    ratings = ''
-    for (i = 1; i <= rating; + + i){
-        ratings += "*"
-    } if (i !== ratings) {
-        ratings += " "
-    }
-    return ratings
+    
 }
 
 console.log (showRating(4))
