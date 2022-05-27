07b3ce7d06528ef8b12cfbbd59b846d0f60d4360
Ryan Norin <nironr6@gmail.com>
2022-05-27 14:17:30 +1000

    changes

--- JSCrashCourse/JS Sample Website/JS Sample/app.js
+++ JSCrashCourse/JS Sample Website/JS Sample/app.js
@@ -1,5 +1,11 @@
-function returnString(str) {
-    return str.split('').reverse('').join('')
+function showRating(rating) {
+    let ratings = ""
+    for (i = 0; i < rating; ++i) {
+    ratings += "*"
+    } if (i !==rating) {
+        ratings += " "
+    }
+    return ratings
 }
 
-console.log(returnString('abc'))
+console.log (showRating(4))
