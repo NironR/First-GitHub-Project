1c2596c23a4b21cc7b848e004c0419a4b20c599e
Ryan Norin <nironr6@gmail.com>
2022-05-27 18:44:07 +1000

    New changes

--- JSCrashCourse/JS Sample Website/JS Sample/app.js
+++ JSCrashCourse/JS Sample Website/JS Sample/app.js
@@ -1,5 +1,15 @@
-function showRating(rating){
-    
+function showRating(rating) {
+    let ratings = ''
+    for (i = 1; i <= rating; ++i) {
+        ratings += '*'
+        if (i !== Math.floor(rating)) {
+            ratings += " "
+        }
+    }
+    if (!Number.isInteger(rating)) {
+        ratings += " ."
+    }
+    return ratings
 }
 
-console.log (showRating(4))
+console.log(showRating(1))
