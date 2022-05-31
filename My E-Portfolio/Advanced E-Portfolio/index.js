396f94d7ec32ddbe7a358b0be2ff44631d2b3fd7
Ryan Norin <nironr6@gmail.com>
2022-05-31 11:53:25 +1000

    Update 0.4

---
+++ My E-Portfolio/Advanced E-Portfolio/index.js
@@ -0,0 +1,32 @@
+
+async function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    
+    emailjs
+        .sendForm(  
+            'service_cs3sb0l',
+            'template_q18zeye',
+            event.target,
+            'dq6a02WxceH5aCcYK'
+    ).then(() =>  {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on nironr6@gmail.com"
+        )
+    })
+
+    
+    
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+        
+        console.log ('it worked 1')
+    }, 1000)
+
+}
