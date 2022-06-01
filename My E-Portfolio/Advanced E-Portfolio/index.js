c67e749b61bb5cb86e6a1fd71298e07bc6e3dad5
Ryan Norin <nironr6@gmail.com>
2022-06-01 10:54:22 +1000

    Update 0.6

--- My E-Portfolio/Advanced E-Portfolio/index.js
+++ My E-Portfolio/Advanced E-Portfolio/index.js
@@ -1,23 +1,34 @@
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector(".modal__overlay--loading");
-    const success = document.querySelector(".modal__overlay--success");
-    loading.classList += " modal__overlay--visible";
-    emailjs
-      .sendForm(
-        "service_cs3sb0l",
-        "template_q18zeye",
-        event.target,
-        "dq6a02WxceH5aCcYK"
-      )
-      .then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-      })
-      .catch(() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert(
-          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
-        );
-      });
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_cs3sb0l",
+      "template_q18zeye",
+      event.target,
+      "dq6a02WxceH5aCcYK"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on nironr6@gmail.com"
+      );
+    });
+}
+
+let isModalOpen = false
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false
+    return document.body.classList.remove ("modal--open")
   }
+  isModalOpen = true
+  document.body.classList += " modal--open"
+}
