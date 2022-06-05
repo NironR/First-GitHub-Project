4dbdab5d62b8975b5312438eab3f1fbcb1de9822
Ryan Norin <nironr6@gmail.com>
2022-06-05 11:42:11 +1000

    Update

--- My Websites/My Websites/Random/user-posts-starter-20220602T234452Z-001/user-posts-starter/user.js
+++ My Websites/My Websites/Random/user-posts-starter-20220602T234452Z-001/user-posts-starter/user.js
@@ -1,7 +1,7 @@
 const postListEl = document.querySelector('.post-list')
 
 async function onSearchChange(event) {
-  console.log(event.target.value)
+  const id = event.target.value
   const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
   const postsData = await posts.json()
   postListEl.innerHTML = postsData.map (post =>
@@ -17,21 +17,21 @@ async function onSearchChange(event) {
 }
 
 async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
-    const postsData = await posts.json()
-    console.log(postsData)
+  const id = localStorage.getItem("id")
+  const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
+  const postsData = await posts.json()
+  console.log(postsData)
 
-    postListEl.innerHTML = postsData.map (post =>
-        `<div class="post">
-        <div class="post__title">
-          ${post.title}
-        </div>
-        <p class="post__body">
-          ${post.body}
-        </p>
-      </div>`
-      ).join('')
+  postListEl.innerHTML = postsData.map (post =>
+      `<div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>`
+    ).join('')
 }
 
 main()
