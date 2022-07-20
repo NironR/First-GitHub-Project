77473d337588c6f365d609a2c41df2faf8719732
Ryan Norin <nironr6@gmail.com>
2022-07-20 17:19:54 +1000

    commiti

--- My Websites/My Websites/Projects/twitter-clone-app/src/components/Tweetbox.jsx
+++ My Websites/My Websites/Projects/twitter-clone-app/src/components/Tweetbox.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tweetbox.css'
 import { Avatar, Button } from '@mui/material'
 import '../App.css'
 
 const Tweetbox = () => {
+    const [tweetMessage, setTweetMessage] = useState('')
+
     return (
         <div className='tweetbox'>
             <form action="">
                 <div className="tweetbox__input">
                     <Avatar />
-                    <input type="text" placeholder="What's happening?"/>
+
+                    <input 
+                    onChange={(e) => setTweetMessage(e.target.value)}
+                    type="text" 
+                    placeholder="What's happening?"
+                    value={tweetMessage}/>
+                    
                 </div>
                 <input className="tweetbox__input--img" placeholder="Optional: Enter image URL"/>
                 <Button className='tweetbox__tweetbtn'>Tweet</Button>
