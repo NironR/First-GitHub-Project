c358d7bd7b18a14689c6125daa002edc7df94b23
Ryan Norin <nironr6@gmail.com>
2022-07-25 08:56:02 +1000

    commit

--- My Websites/My Websites/Projects/twitter-clone-app/src/components/Tweetbox.jsx
+++ My Websites/My Websites/Projects/twitter-clone-app/src/components/Tweetbox.jsx
@@ -2,9 +2,29 @@ import React, { useState } from 'react';
 import './Tweetbox.css'
 import { Avatar, Button } from '@mui/material'
 import '../App.css'
+import { db }from '../fiirebase/init'
+import { collection, addDoc } from "firebase/firestore"
 
 const Tweetbox = () => {
     const [tweetMessage, setTweetMessage] = useState('')
+    const [tweetImage, setTweetImage] = useState('')
+   
+    async function sendTweet(e){
+        e.preventDefault()
+        const post = {
+            displayName: "Ryan Norin",
+            username: "niron_r",
+            verified: true,
+            text: tweetMessage,
+            image: tweetImage,
+            avatar: 
+            "https://static.wikia.nocookie.net/1e7fa32e-67a2-4360-a0e8-415d16daf44e/scale-to-width/755"
+
+        }
+        setTweetMessage("")
+        setTweetImage("")
+        await addDoc(collection(db, "posts"), post)
+    } 
 
     return (
         <div className='tweetbox'>
@@ -19,8 +39,15 @@ const Tweetbox = () => {
                     value={tweetMessage}/>
                     
                 </div>
-                <input className="tweetbox__input--img" placeholder="Optional: Enter image URL"/>
-                <Button className='tweetbox__tweetbtn'>Tweet</Button>
+                <input 
+                    value={tweetImage}
+                    onChange={e => setTweetImage(e.target.value)}
+                    className="tweetbox__input--img" 
+                    placeholder="Optional: Enter image URL"/>
+                <Button 
+                    onClick={sendTweet} 
+                    type="submit" 
+                    className='tweetbox__tweetbtn'>Tweet</Button>
             </form>
         </div>
     );
