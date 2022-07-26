34341100f265aa054a094a10186732d95dcb959a
Ryan Norin <nironr6@gmail.com>
2022-07-26 11:26:58 +1000

    commit

--- My Websites/My Websites/Projects/discord-clone-app/src/Components/Chat.jsx
+++ My Websites/My Websites/Projects/discord-clone-app/src/Components/Chat.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Chat.css'
 import ChatHeader from './ui/ChatHeader';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import CardGiftcardRoundedIcon from '@mui/icons-material/CardGiftcardRounded';
+import GifRoundedIcon from '@mui/icons-material/GifRounded';
+import EmojiEmotionsRoundedIcon from '@mui/icons-material/EmojiEmotionsRounded';
 
 const Chat = () => {
     return (
         <div className='chat'>
             <ChatHeader />
+
+            <div className="chat__messages">
+
+            </div>
+
+            <div className="chat__input">
+                <AddCircleRoundedIcon fontSize='large'/>
+                <form action="">
+                    <input type="text" placeholder='message #test-channel' />
+                    <button className='chat__input--btn'  type='submit'>Send Message</button>
+                </form>
+
+                <div className="chat__input--icons">
+                    <CardGiftcardRoundedIcon fontSize='large'/>
+                    <GifRoundedIcon fontSize='large'/>
+                    <EmojiEmotionsRoundedIcon fontSize='large'/>
+                </div>
+            </div>
         </div>
     );
 }
--- My Websites/My Websites/Projects/discord-clone-app/src/Components/ui/ChatHeader.jsx
+++ My Websites/My Websites/Projects/discord-clone-app/src/Components/ui/ChatHeader.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './ChatHeader.css'
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import EditLocationRoundedIcon from '@mui/icons-material/EditLocationRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
 
 const ChatHeader = () => {
     return (
         <div className='chat__header'>
-            
+            <div className="chat__header--left">
+                <h3>
+                    <span className="chat__header--hash">
+                        #
+                    </span>
+                    Text Channel Name
+                </h3>
+            </div>
+
+            <div className="chat__header--right">
+                <NotificationsIcon />
+                <EditLocationRoundedIcon />
+                <PeopleAltRoundedIcon />
+
+                <div className="chat__header--search">
+                    <input type="text" placeholder='Search' />
+                    <SearchRoundedIcon />
+                </div>
+                <SendRoundedIcon />
+                <HelpRoundedIcon />
+            </div>
         </div>
     );
 }
