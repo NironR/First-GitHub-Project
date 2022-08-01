10bb36e55036f60ef00e52294efe327140fa2645
Ryan Norin <nironr6@gmail.com>
2022-08-01 11:44:44 +1000

    commit

--- My Websites/My Websites/Projects/mock-interview-project/src/App.js
+++ My Websites/My Websites/Projects/mock-interview-project/src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Post from './components/Post';
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path=':id' element={<Post />}></Route>
+        </Routes>
+      </div>
+    </Router>
+    
   )
 }
 
